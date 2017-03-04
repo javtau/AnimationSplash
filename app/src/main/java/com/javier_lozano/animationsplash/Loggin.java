e71bc01fb84b06634eb250f19e46ef2b4912c095
javtau <javier.lozano.almeda@gmail.com>
2017-03-04 02:17:38 +0100

    2

---
+++ app/src/main/java/com/javier_lozano/animationsplash/Loggin.java
@@ -0,0 +1,13 @@
+package com.javier_lozano.animationsplash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Loggin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loggin);
+    }
+}
